2ef6a44618754027440e5dbd1a841b8bb0ae8c78
shmhiri <saddemhcine.mhiri@esprit.tn>
2021-01-24 04:48:35 +0100

    second commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :lockable, :confirmable, :validatable
+         :recoverable, :rememberable, :validatable
 end
